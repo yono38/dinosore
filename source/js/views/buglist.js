98b753f679d3db0da0070ea08d37c7ccb83bf76a
Jason Schapiro <yono38@gmail.com>
2013-06-19 10:12:55 -0400

    got radio buttons working

--- source/js/views/buglist.js
+++ source/js/views/buglist.js
@@ -7,8 +7,7 @@ window.BugListView = Backbone.View.extend({
         this.collection = new BugList();
     //    this.collection.on("all", this.render);
         this.collection.on("add", this.addOne);
-        
-        
+           
     },
     
     events: {
@@ -78,23 +77,60 @@ window.BugItemView = Backbone.View.extend({
 window.NewBugView = Backbone.View.extend({
   initialize: function(){
     this.template = _.template(tpl.get('add-bug'));  
+    this.first = true;   
+    this.priority = 1;
   },
   events: {
-
+    "click #addBtn" : "addBug",
+    "create": "changePriority"
+  },
+  changePriority: function(){
+    var that = this;
+    $(this).click(function(){ 
+      that.priority = $(this).val();
+      console.log(that.priority);
+    });
+  },
+  addBug: function(){
+    var bug = new Bug({
+      bugPriority: this.priority,
+      user: Parse.User.current()
+    });
+    if (this.$("#bugtitle").val() != ""){
+      bug.set("title", this.$("#bugtitle").val());
+    }
+    if (this.$("#bugdetails").val() != ""){
+      bug.set("bugDetails", this.$("#bugdetails").val());    
+    }
+    if (this.$("#assignedto").val() != ""){
+      bug.set("doctor", this.$("#assignedto").val());    
+      bug.set("bugStatus", "Assigned");          
+    }
+    bug.save(null, {
+      success: function(bug){
+        console.log("New bug saved: "+bug.id);
+        app.navigate("list", {trigger: true});
+      },
+      error: function(bugg, error){
+        console.log("Failed to save bug, error: "+error.description);
+        console.log(error);
+      }
+    });
   },
   preventDefault: function(e){
     console.log(($(this)[0]).el);
     $(($(this)[0]).el).css("background", "red");
     e.preventDefault();
   },
-  choosePriority: function(e){
-    console.log($(this));
-    
-   // console.log($(this).css("background", "red"));
-   // $(this).checkboxradio('enable');
-  },
   render: function(){
     $(this.el).html(this.template());
+    var that = this;
+    if (this.first){
+      this.first = false;
+      $(this.el).find("input[type='radio']").checkboxradio({
+       create: that.changePriority
+      });
+    }
     return this;  
   }
   
