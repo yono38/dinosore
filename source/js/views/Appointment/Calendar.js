d5da083f1407316f49673900129793c9c2f6d075
Jason Schapiro <yono38@gmail.com>
2013-08-17 14:45:21 -0400

    fixed sizing of appointments page

--- source/js/views/Appointment/Calendar.js
+++ source/js/views/Appointment/Calendar.js
@@ -38,7 +38,7 @@ window.AppointmentsView = Backbone.View.extend({
                     $("#fakeAppt").html(_.template(tpl.get("appointment-item"), apptData)); 
                     $("#fakeAppt").show();
                     $("#noAppt").hide();
-                    
+
                     this.$(".removeAppt").button();
                     this.$(".editAppt").button();
                   },
@@ -60,6 +60,8 @@ window.AppointmentsView = Backbone.View.extend({
         setTimeout( function(){
             $("#mydate").datebox({"mode": "calbox", "highDates": that.highDates,  "useInline": true, "useImmediate":true, hideInput:true, calHighToday: false}); 
         }, 200);
+        	// get rid of annoying background shadow
+        	setTimeout(function() {$(".ui-input-text.ui-shadow-inset").css({"border":"none", "box-shadow": "none"});}, 100);
         this.$("#checkbox-6").on( "checkboxradiocreate", function( event, ui ) {
           $(this).bind("click", that.addAppt);
         });
@@ -138,122 +140,4 @@ window.AppointmentsView = Backbone.View.extend({
       $("#noAppt").show();
     }
 
-});
-
-window.NewApptView = Backbone.View.extend({
-  initialize: function(){
-    this.template = _.template(tpl.get('app'));  
-    this.first = true;   
-    this.priority = 1;
-  },
-  events: {
-    "click #addBtn" : "addAppt",
-    "click" : "navBtn"
-  },
-  
-  navBtn: function(e){
-  	
-  	console.log(e);
-  },
-  
-  addAppt: function(e){
-    e.preventDefault();
-    
-    var appt = new Appointment({
-      user: Parse.User.current(),
-      date: this.$("#appt-date").val()
-    });
-    if (this.$("#title").val() != ""){
-      appt.set("title", this.$("#title").val());
-    }
-    if (this.$("#select-bug").val() != "none"){
-      appt.set("bug", this.$("#select-bug").val());    
-    }
-    if (this.$("#select-doc").val() != "none"){
-      appt.set("doc", this.$("#select-doc").val());    
-    }
-    appt.set("newDate", 
-      moment(
-        $("#appt-date").val()+" "+$("#appt-time").val()
-      ).toDate()
-    );
-    
-    appt.save(null, {
-      success: function(appt){
-        console.log("New appt saved: "+appt.id);
-        app.navigate("appts", {trigger: true});
-      },
-      error: function(appt, error){
-        console.log("Failed to save appt, error: "+error.description);
-        console.log(error);
-      }
-    });
-  },
-  
-  // courtesy of o-o on stackoverflow
-  yyyymmdd : function() {
-   var d = new Date();
-   var yyyy = d.getFullYear().toString();
-   var mm = (d.getMonth()+1).toString(); // getMonth() is zero-based
-   var dd  = d.getDate().toString();
-   return yyyy +"-"+ (mm[1]?mm:"0"+mm[0]) +"-"+ (dd[1]?dd:"0"+dd[0]); // padding
-  },
-  
-  yyyymmdd_SlashConvert: function(str){
-    var split = str.split('-');
-    return split[1]+'/'+split[2]+'/'+split[0];
-  },
-  
-  preventDefault: function(e){
-    e.preventDefault();
-  },
-  
-  render: function(){
-    var todayDate = new Date();
-    var data = { 
-     "today" : this.yyyymmdd()
-    };
-    $(this.el).html(this.template(data));
-    this.renderMenu("bug");
-    this.renderMenu("doctor");    
-    return this;  
-  },
-  
-  // for bugs or doctors
-  renderMenu: function(type){
-    if (!(type == "doctor" || type == "bug")) {
-      console.log("invalid type: "+type);
-      return;
-    }
-    var item, query, selector;
-    if (type == "bug") {
-      item = new BugList();
-      query = Bug;
-      selector = "#select-bug";
-    } else {
-      item = new DoctorList();
-      query = Doctor;
-      selector = "#select-doc";
-    }
-    item.query = new Parse.Query(query);
-    if (type == "bug") {
-      item.query.equalTo("user", Parse.User.current());
-    }
-    var that = this;
-    item.fetch({
-      success: function(coll){
-        console.log(coll);
-        that.$(selector).html("<option value='none'>None</option>");        
-        _.each(coll['_byId'], function(v,k){
-          that.$(selector).append("<option value='"+k+"'>"+v.get("title")+"</option>");
-        });
-       // TODO add new item from menu
-       // that.$(selector).append("<option id='newBug'>New Bug</option>");
-      },  
-      error: function(coll, err){
-        console.log(err);
-      }
-    });
-  }
-  
 });
