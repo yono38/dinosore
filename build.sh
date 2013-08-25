6e413a1feac2154b2ab3bfcfcfc952a8d59d752f
Jason <jason@jason-N56VM.(none)>
2013-08-24 20:48:07 -0400

    possible deploy script working

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "Pushing to git..."
+git push origin master
+echo "Pushing to jasonschapiro.com..."
+scp -r source/ root@jasonschapiro.com:/var/www/InternalProjects/dinosore/
 DATE=$(date +"%Y%m%d%H%M")
 echo "Copying files to phonegap directory..."
 cp -u -v -r source/. my-app/www/ >> logs/build_$DATE.log
-echo "Log written to logs/build_$DATE.log"
+echo "File copy successful, log written to logs/build_$DATE.log"
 echo "Zipping file..."
-
+zip -r my-app my-app
+echo "File zipped successfully"
+echo "Running node script..."
+node deploy.js
--- deploy.js
+++ deploy.js
@@ -5,78 +5,77 @@
 var fs = require('fs');
 var nodemailer = require('nodemailer');
 var client = require('phonegap-build-api');
-var sys = require('sys')
-var zip = require('adm-zip');
 
-// copy files into phonegap folder
-var exec = require('child_process').exec;
-var child = exec("cp -u -v -r source/. my-app/www/ >> logs/build_"+(new Date().getTime())+'.txt', function (error, stdout, stderr) {
-	console.log("Files copied - log saved to logs/build_"+(new Date().getTime())+'.txt');
-	
-});
-
-
-/********* BUILD THE APP ***************/
-function buildApp(){
-// download the app from phonegap build
-client.auth({ token: 'wnANkqxtFzbqzQ6sMxyx' }, function(e, api) {
-zip.addLocalFolder('my-app/');
-zip.writeZip('my-app.zip', function(
-
-});
-}
-
-/********* ALERT THE TEAM ************/
-function sendMail(){
-// Create a SMTP transport object
-var transport = nodemailer.createTransport("SMTP", {
-        //service: 'Gmail', // use well known service.
-                            // If you are using @gmail.com address, then you don't
-                            // even have to define the service name
-        auth: {
-            user: "dinosorehealth@gmail.com",
-            pass: "2bearsbakery"
-        }
-    });
-
-console.log('SMTP Configured');
-
-// Message object
-var message = {
-    // sender info
-    from: 'Dinosore <jason@dinoso.re>',
-    // Comma separated list of recipients
-    to: '"Jason Schapiro" <yono38@gmail.com>,"Rebecca Hillegass" <rebecca.hillegass@gmail.com>',
-    // Subject of the message
-    subject: 'Phonegap build', //
-    // plaintext body
-    text: 'Built at '+(new Date().getTime()),
-	attachments: [
-		{
-			filePath: "app.apk"
-		}
-	]
-};
-
-// download the app from phonegap build
-client.auth({ token: 'wnANkqxtFzbqzQ6sMxyx' }, function(e, api) {
-	  var writeStream = fs.createWriteStream('app.apk');
-	  var get = (api.get('/apps/514031/android'));
-		get.pipe(writeStream);
-		get.on('end', function(){
-			console.log('file app.apk ready');
-				console.log('Sending Mail');
-				// send the email with the app attached
-				transport.sendMail(message, function(error){
-						if(error){
-								console.log('Error occured');
-								console.log(error.message);
-								return;
-						}
-						console.log('Message sent successfully!');
-						transport.close(); // close the connection pool
-						return;
+var sendMail = function() {
+				// Create a SMTP transport object
+				var transport = nodemailer.createTransport("SMTP", {
+								//service: 'Gmail', // use well known service.
+																		// If you are using @gmail.com address, then you don't
+																		// even have to define the service name
+								auth: {
+										user: "dinosorehealth@gmail.com",
+										pass: "2bearsbakery"
+								}
 				});
-		});
-});
+
+				// Message object
+				var message = {
+						// sender info
+						from: 'Dinosore <jason@dinoso.re>',
+						// Comma separated list of recipients
+						to: '"Jason Schapiro" <yono38@gmail.com>,"Rebecca Hillegass" <rebecca.hillegass@gmail.com>',
+						// Subject of the message
+						subject: 'Phonegap build', //
+						// plaintext body
+						text: 'Built at '+(new Date().getTime()),
+					attachments: [
+						{
+							filePath: "app.apk"
+						}
+					]
+				};
+
+				/********* AUTHORIZE WITH PHONEGAP ***************/
+				client.auth({ token: 'wnANkqxtFzbqzQ6sMxyx' }, function(e, api) {
+						var writeStream = fs.createWriteStream('app.apk');
+						/********* DOWNLOAD THE APP ***************/
+						var get = (api.get('/apps/514031/android'));
+						get.pipe(writeStream);
+						get.on('end', function(){
+							console.log('file app.apk ready');
+								console.log('Sending Mail');
+								/********* ALERT THE TEAM ***************/
+								// send the email with the app attached
+								transport.sendMail(message, function(error){
+										if(error){
+												console.log('Error occured');
+												console.log(error.message);
+												return;
+										}
+										console.log('Message sent successfully!');
+										transport.close(); // close the connection pool
+										return;
+								});
+						});
+				});
 }
+
+
+client.auth({ token: 'wnANkqxtFzbqzQ6sMxyx' }, function(e, api) {
+  var options = {
+    form: {
+        data: {
+            title: 'Dinosore',
+            create_method: 'file'
+        },
+        file: 'my-app.zip'
+    }
+  };
+
+  api.post('/apps/514031/build/android', options, function(e, data) {
+    console.log('error:', e);
+    console.log('data:', data);
+	  console.log("Build successful");
+		sendMail();
+  });
+});
