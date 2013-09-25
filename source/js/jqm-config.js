39ab51669709332e75e1b3ab4d56a6aeee0405d4
Jason S <yono38@gmail.com>
2013-09-25 19:27:51 -0400

    trying to remove timeout problems

--- source/js/jqm-config.js
+++ source/js/jqm-config.js
@@ -4,6 +4,8 @@ $(document).bind("mobileinit", function () {
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
     $.mobile.pushStateEnabled = false;
+    $.support.cors = true;
+	$.mobile.allowCrossDomainPages = true;
 
     // Remove page from DOM when it's being replaced
     $('div[data-role="page"]').live('pagehide', function (event, ui) {
