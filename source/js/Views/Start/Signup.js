f7245c50f619d04944968737ee9572639d4565d3
Jason Schapiro <yono38@gmail.com>
2013-10-12 11:12:16 -0400

    work with startup

--- source/js/Views/Start/Signup.js
+++ source/js/Views/Start/Signup.js
@@ -6,6 +6,32 @@ window.$dino.StartSignupView = Backbone.View.extend({
 	events : {
 	},
 
+	validateFieldsExist: function(){
+		
+	},
+  
+  signup: function(){
+    var usr = this.$("#email").val();
+    var pw = this.$("#password").val();
+    var that = this;
+   if (usr == "" || pw == ""){
+    	this.$("#error").html("Email & Password Cannot Be Blank");
+    	return;
+    }
+    Parse.User.signUp(usr, pw, {}, {
+      success: function(user) {
+      	var User = $dino.User(Parse.User.current().toJSON());
+      	User.set("id", Parse.User.current().id);
+      	User.save();
+        $dino.app.navigate("symptoms", {trigger: true});
+      }, 
+      error: function(err){
+        that.$("#error").html(err.message.toTitleCase());
+      }
+    });
+  },
+  
+
 	render : function(eventName) {
 		this.$el.html(this.template());
 		return this;
