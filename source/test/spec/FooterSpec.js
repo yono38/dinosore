c4ee31f6f6a1e658c5c6d384f738f5ad432692ee
Danny Rosen <danny.rosen@gmail.com>
2013-12-15 22:10:17 -0500

    adding footerspec tests

--- source/test/spec/FooterSpec.js
+++ source/test/spec/FooterSpec.js
@@ -1,18 +1,22 @@
-describe("FooterView", function(){
-	var view;
-	
-	beforeEach(function(){
-		view = new $dino.FooterView();
-	});
-	
-	afterEach(function(){
-		view.destroy();
-	});
-	
-	it("renders", function(){
-		view.render();
-		expect(view.$el).not.toBeEmpty();
-	});
-	
-	
+describe("FooterTest", function(){
+    var view;
+    beforeEach(function(){
+        view = new FooterView();
+    });
+    
+    it("contains the right quantity", function(){
+        view.render(); 
+        expect(view.$("#footerBtnGroup #bugs").length).toEqual(4); 
+    });
+
+    it("contains the specific footer items", function(){
+        view.render(); 
+        expect(view.$el).toBe('div'); 
+        expect(view.$("#footerBtnGroup")).toContain($("a[href=bugs]")); 
+        expect(view.$("#footerBtnGroup")).toContain($("#medication")); 
+        expect(view.$("#footerBtnGroup")).toContain($("#appointments"));
+        expect(view.$("#footerBtnGroup")).toContain($("#info")); 
+    });
+    
+    
 });
