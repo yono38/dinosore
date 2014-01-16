d888d3e5ee53443bf82add89573502813cfa6e16
Jason Schapiro <yono38@gmail.com>
2014-01-16 00:17:35 -0500

    modified footerspec test

--- source/test/spec/FooterSpec.js
+++ source/test/spec/FooterSpec.js
@@ -1,18 +1,23 @@
-describe("FooterView", function(){
-	var view;
-	
-	beforeEach(function(){
-		view = new $dino.FooterView();
-	});
-	
-	afterEach(function(){
-		view.destroy();
-	});
-	
-	it("renders", function(){
-		view.render();
-		expect(view.$el).not.toBeEmpty();
-	});
-	
-	
+describe("FooterTest", function(){
+    var view;
+    // instantiate the view
+    // So before each test begins we instantiate a new footerview and assign it to the view object - correcto
+    beforeEach(function(){
+        view = new $dino.FooterView();
+    });
+    
+    it("contains the footer items", function(){
+        // see https://github.com/velesin/jasmine-jquery
+        // run render
+        view.render(); // we call the render function (? method) on the view object - as required for the test :)
+        expect(view.$el).toBe('div'); // finish this statement danny?
+        expect(view.$("#footerBtnGroup a").length).toEqual(4); // (as there are 4 items)
+        expect(view.$("#footerBtnGroup")).toContain("a[href=#bugs]"); // correct
+        expect(view.$("#footerBtnGroup")).toContain("#medications"); // this one was (ALMOST) correct (look at ID again), look at the <a> element in tpl and see why AH I MISSED the ID. got it
+        expect(view.$("#footerBtnGroup")).toContain("#appointments"); // #fixed YEP On an unrelated note, I should have made this route #appointments anyway :P
+        expect(view.$("#footerBtnGroup")).toContain("#info") // #info
+        
+    });
+    
+    
 });
